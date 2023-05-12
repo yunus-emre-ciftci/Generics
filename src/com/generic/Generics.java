d9f53736d5dd000144101c5a0f8b1381e2f0b97a
Yunus Emre Çiftçi <118928402+yunus-emre-ciftci@users.noreply.github.com>
2023-05-12 22:16:00 +0300

    m

---
+++ src/com/generic/Generics.java
@@ -0,0 +1,29 @@
+package com.generic;
+
+public class Generics {
+    public static void main(String[] args) {
+
+        test1();
+    }
+
+    public static void test1() {
+        /* generic basit kullanım: class, method, List, Arrays
+        GenericUsefulClass sınıfı <G> tipinden referans alan bir sınıftır. Bu sayede başka sınıflarda kullanılırken
+        farklı veri türleriyle referans edebiliriz. İlla G harfi olmak zorunda değil herhangi bir keyword olmayan kelime
+        olabilir.
+        */
+
+        // İki nesnenin de tipini class adının yanına yazarak belirledik
+        GenericsUsefulClass<String> genericsUsefulClass = new GenericsUsefulClass<>("Çok güzel");
+        GenericsUsefulClass<Integer> genericsUsefulClass1 = new GenericsUsefulClass<>(3);
+        genericsUsefulClass.getData();
+        genericsUsefulClass1.getData();
+
+
+        // Burada nesne oluşturuken refere edecek bir şey yazmadık. Hata verecek. Çünkü GenericsUsefulClass G tipinden referans almaktadır. Neyden referans
+        // aldığı belirtilmezse hata alınacaktır. Java nasıl bir nesne oluşturacağını bilemez.
+        // GenericsUsefulClass genericsUsefulClass2 = new GenericsUsefulClass<>();
+        //genericsUsefulClass2.getData();
+    }
+
+}
---
+++ src/com/generic/GenericsUsefulClass.java
@@ -0,0 +1,30 @@
+package com.generic;
+
+public class GenericsUsefulClass<G> {
+
+    private G powerful;
+
+    public GenericsUsefulClass(G powerful) {
+        this.powerful = powerful;
+    }
+    public GenericsUsefulClass(){
+
+    }
+
+    public G getPowerful() {
+        return powerful;
+    }
+
+    public void setPowerful(G powerful) {
+        this.powerful = powerful;
+    }
+
+    public void getData() {
+        System.out.println(powerful.getClass().getName());
+    }
+
+
+}
+
+
+
